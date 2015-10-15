2e730c5b52f60b4af6f019bd36943f3d08f2f63b
max takano <maxytakano@gmail.com>
2015-10-14 19:50:00 -0700

    beginning check 8a

--- starterCode/src/ArithmeticOp.java
+++ starterCode/src/ArithmeticOp.java
@@ -40,18 +40,32 @@ class ArithmeticOp extends BinaryOp
         } else if ( !(bType.isNumeric()) ) {
             return new ErrorSTO( Formatter.toString(ErrorMsg.error1n_Expr, bType.getName(), getName()) );
         } else if (aType.isInt() && bType.isInt()) {
-
             StringBuilder expr_builder = new StringBuilder();
             expr_builder.append(a.getName()).append(getName()).append(b.getName());
 
-            ExprSTO expr = new ExprSTO(expr_builder.toString(), new IntType());
+            STO expr;
+            if (a.isConst() && b.isConst()) {
+                // both are const, return a const expr.
+                expr = new ConstSTO(expr_builder.toString(), new IntType());
+            } else {
+                // if any are var return a expr.
+                expr = new ExprSTO(expr_builder.toString(), new IntType());
+            }
+
             return expr;
         } else {
-
             StringBuilder expr_builder = new StringBuilder();
             expr_builder.append(a.getName()).append(getName()).append(b.getName());
 
-            ExprSTO expr = new ExprSTO(expr_builder.toString(), new FloatType());
+            STO expr;
+            if (a.isConst() && b.isConst()) {
+                // both are const, return a const expr.
+                expr = new ConstSTO(expr_builder.toString(), new FloatType());
+            } else {
+                // if any are var return a expr.
+                expr = new ExprSTO(expr_builder.toString(), new FloatType());
+            }
+
             return expr;
         }
     }
